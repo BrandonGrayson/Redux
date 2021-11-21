55aea9c68b858c1cb0f45ecfadc31412065b5acf
Brandon <brandonbgrayson@gmail.com>
2021-11-21 15:05:17 -0500

    updated createStore with reducer todos function

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-function createStore() {
+// {
+//     type: 'ADD_TODO',
+//         todo: {
+//         id: 0,
+//         name: 'Learn Redux',
+//         complete: false,
+//     }
+// }
+
+// {
+//     type: 'REMOVE_TODO',
+//         id: 0,
+//   }
+
+// {
+//     type: 'TOGGLE_TODO',
+//         id: 0,
+//   }
+
+// {
+//     type: 'ADD_GOAL',
+//         goal: {
+//         id: 0,
+//             name: 'Run a Marathon'
+//     }
+// }
+
+// {
+//     type: 'REMOVE_GOAL',
+//         id: 0
+// }
+
+function todos(state, action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+}
+
+
+function createStore(reducer) {
+
     let state;
     let listeners = []
 
     const getState = () => state
-    
+
     const subscribe = (listener) => {
         listeners.push(listener)
 
