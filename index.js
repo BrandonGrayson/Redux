233d8cca2035828677d351b60fb663bd1555d88f
Brandon <brandonbgrayson@gmail.com>
2021-11-22 13:16:45 -0500

    refactored if statement inside todos reducer function to use a swtich case statement

--- index.js
+++ index.js
@@ -31,6 +31,19 @@
 // }
 
 function todos(state = [], action) {
+    switch(action.type) {
+        case 'ADD_TODO' :
+            return state.concat([action.todo])
+        case 'REMOVE_TODO' :
+            return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO' :
+            return state.filter((todo) => todo.id !== action.id ? todo :
+                Object.assign({}, todo, {complete: !todo.complete})
+            )
+    }
+
+
+
     if (action.type === 'ADD_TODO') {
         return state.concat([action.todo])
     } else if (action.type === 'REMOVE_TODO') {
